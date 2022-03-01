238753882c4cee8176fe27d3867437c01f8ade75
Kruna1Pate1 <kbpatel5802@gmail.com>
2022-03-01 23:06:58 +0530

    Initial commit

---
+++ server/src/main/java/com/kruna1pate1/bookesale/BookESaleApplication.java
@@ -0,0 +1,13 @@
+package com.kruna1pate1.bookesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookESaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookESaleApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/kruna1pate1/bookesale/BookESaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruna1pate1.bookesale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookESaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
